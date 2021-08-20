96acd2764a2485a214884bac3a36101c7cf2e8f9
Cristian Hourcade <hourcadecristian@gmail.com>
2021-08-19 22:39:02 -0300

    Clase 6 - Arrrays y carrito

--- js/main.js
+++ js/main.js
@@ -3,22 +3,13 @@ const producto_dos = new Producto('Zapas Adidas', 100, 10, "https://www.purina-l
 const producto_tres = new Producto('Zapas de Solodeportes.com.ar', 700, 10, "https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/purina-reconocer-a-un-perro-feliz.jpg?itok=8xw_Cyof");
 const producto_cuatro = new Producto('Zapas Jagguar', 1200, 10, "https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/purina-reconocer-a-un-perro-feliz.jpg?itok=8xw_Cyof");
 const producto_cinco = new Producto('Zapas Jagguar', 1200, 10, "/img/perro.jpg");
-                        //0             //1            //2            //3
+
 const baseDeDatos = [producto_uno, producto_dos, producto_tres, producto_cuatro, producto_cinco];
 
-// let acumulador = ``;
-// for(let i = 0; i<baseDeDatos.length;i++){ 
-//     acumulador += `<div class="card" style="background-color:violet">
-//     <img src="${baseDeDatos[i].img}" width="70" height="70"/>
-//     <h3>${baseDeDatos[i].title}</h3>
-//     <p>$${baseDeDatos[i].price}</p>
-//     </div>`
-// }
-let iterable = [10, 20, 30];
-for (let value of iterable) {
-    value += 1;
-    console.log(value);
-  }
+const baseDeDatosMenosDe200 = baseDeDatos.filter(producto => producto.price < 1300);
+console.log(baseDeDatosMenosDe200);
+
+const carrito = [];
 
 let acumulador = ``;
 baseDeDatos.forEach((producto) => {
@@ -26,8 +17,40 @@ baseDeDatos.forEach((producto) => {
     <img src="${producto.img}" width="70" height="70"/>
     <h3>${producto.title}</h3>
     <p>$${producto.price}</p>
+    <button onclick="agregarAlCarrito('${producto.title}')">Agregar al carrito</button>
     </div>`
 });
 
 document.write(acumulador);
 
+function agregarAlCarrito(title){
+    // TODO: En vez de un titulo, agregar el producto que se agrego.
+    // TODO: Verificar stock del producto
+    const productoEncontrado = baseDeDatos.find(producto => producto.title === title);
+    if(productoEncontrado != undefined){
+        carrito.push(productoEncontrado);
+    }else{
+        alert("algo falló");
+    }
+    // TODO: calcular el total del carrito
+    console.log(carrito)
+}
+
+// const miArray    = ["ford", 45];
+// const otroArray  = ["hola", 22, "mundo"];
+// const nuevoArray = miArray.concat(otroArray);
+// console.log([...miArray, ...otroArray]);
+// console.log(nuevoArray);
+
+// const nombres    = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+// const masculinos = nombres.slice(1, 3);
+// console.log(masculinos)
+
+
+// console.log(1 === 1 === 1);
+
+// const numeros = [1, 2, 3, 4, 5];
+// const porDos = numeros.map(element => {
+//     return element * 2
+// });
+// console.log(porDos);
